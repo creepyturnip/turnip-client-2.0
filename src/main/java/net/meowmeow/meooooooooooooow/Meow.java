4dc614e16d84cca86af851ad895bbfcd0e2c8b34
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2022-12-15 11:43:53 +0100

    Update Meow.java

--- src/main/java/net/meowmeow/meooooooooooooow/Meow.java
+++ src/main/java/net/meowmeow/meooooooooooooow/Meow.java
@@ -1,22 +1,36 @@
 package net.meowmeow.meooooooooooooow;
 import net.fabricmc.api.ModInitializer;
+import net.meowmeow.meooooooooooooow.meows.ModuleManager;
+import net.minecraft.client.MinecraftClient;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.meowmeow.meooooooooooooow.meows.Mod;
 
 public class Meow implements ModInitializer {
 
 	public static final Meow INSTANCE = new Meow();
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	private MinecraftClient MC = MinecraftClient.getInstance();
+
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("well something works!");
 	}
 
 	public void onKeyPress(int key, int action) {
-		if (action == GLFW.GLFW_PRESS) System.out.println("key " + key + " was pressed");
+		if (action == GLFW.GLFW_PRESS) {
+            for (Mod module : ModuleManager.INSTANCE.GetEnabledModules()) {
+				if (key == module.getKey()) module.toggle();
+            }
+        }
 	}
 	public void onTick() {
+		if(MC.player != null) {
+			for (Mod module : ModuleManager.INSTANCE.GetEnabledModules()) {
+				module.onTick();
+			}
+		}
 	}
 }
