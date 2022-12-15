9e92016d4c64a72f11181eadb0d826c1006e2e94
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2022-12-15 11:43:06 +0100

    Update ModuleManager.java

--- src/main/java/net/meowmeow/meooooooooooooow/meows/ModuleManager.java
+++ src/main/java/net/meowmeow/meooooooooooooow/meows/ModuleManager.java
@@ -1,4 +1,30 @@
 package net.meowmeow.meooooooooooooow.meows;
 
+import net.meowmeow.meooooooooooooow.movement.Flight;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModuleManager {
+    public static final ModuleManager INSTANCE = new ModuleManager();
+    private List<Mod> modules = new ArrayList<>();
+
+    public ModuleManager() {
+        addModules();
+    }
+
+    public List<Mod> getModules() {
+        return modules;
+    }
+    public List<Mod> GetEnabledModules() {
+        List<Mod> enabled = new ArrayList<>();
+        for (Mod module : modules) {
+            if (module.isEnabled()) enabled.add(module);
+        }
+
+        return enabled;
+    }
+    private void addModules() {
+        modules.add(new Flight());
+    }
 }
