385dbca037471c2a399aa98d508d6c52e1213f29
creepyturnip <92087207+creepyturnip@users.noreply.github.com>
2022-12-20 13:59:05 +0100

    Update Flight.java

--- src/main/java/net/meowmeow/meooooooooooooow/movement/Flight.java
+++ src/main/java/net/meowmeow/meooooooooooooow/movement/Flight.java
@@ -1,26 +1,28 @@
 package net.meowmeow.meooooooooooooow.movement;
 
+import net.meowmeow.meooooooooooooow.MeowClient;
 import net.meowmeow.meooooooooooooow.meows.Mod;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
+
 
 public class Flight extends Mod {
 
     public Flight() {
         super("Flight", "Makes you fly :D", Category.MOVEMENT);
-        this.setKey(GLFW.GLFW_KEY_N);
+        this.setKey(GLFW.GLFW_KEY_UP);
+
     }
 
     @Override
     public void onTick() {
-        MC.player.getAbilities().allowFlying = true;
-        System.out.println("FLYING");
-        super.onTick();
-    }
-
-    @Override
-    public void onDisable() {
-        MC.player.getAbilities().allowFlying = false;
-        System.out.println("flying disabled :(");
-        super.onDisable();
+        if (MC.player!=null && MC.player.hasVehicle()) {
+            Entity vehicle = MC.player.getVehicle();
+            Vec3d velocity = vehicle.getVelocity();
+            double motionY = MC.options.jumpKey.isPressed() ? 0.5 : 0;
+            vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+        }
     }
 }
